6d4c1a63dd01c3167a0edd61eb5a8de522cf990e
Geoff Kembel <geoffkembel@gmail.com>
2015-04-19 16:41:48 -0600

    First stab on making it responsive: small and large css files with media queries loading the right one in the HTML; increased font-sizes for legibility on small screens; added click to close images in aside; etc

--- js/resume.js
+++ js/resume.js
@@ -40,7 +40,13 @@ $('main a[href$=gif], main a[href$=jpg], main a[href$=png]').each(function() {
     // add new image to aside
     var img = $(document.createElement('img'));
     img.attr('src', $(this).attr('href'));
+    img.attr('title', 'Click to close');
     $('aside').append(img);
+
+    // add click event to close/remove img
+    img.click(function() {
+      $(this).remove();
+    });
 
     // cancel normal link behavior
     return false;
