db3cbc3fc086c24eff6d58eb04523da24ddb9c22
Geoff Kembel <geoffkembel@gmail.com>
2015-04-21 11:11:51 -0600

    Changed css to LESS and added grunt task to compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,32 +4,45 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // clean the build folder
     clean: {
       build: [
         'build'
       ]
     },
 
+    // concat and compile less files
+    less: {
+      build: {
+        files: {
+          "build/compiled.css": ["css/*.less"]
+        }
+      },
+    },
+
+    // concat all js and css files, including the compiled less file
     concat: {
       build: {
         files: {
-          'build/<%= pkg.name %>.js': ['js/*.js'],
-          'build/<%= pkg.name %>.css': ['css/*.css']
+          'build/resume.js': ['js/*.js'],
+          'build/resume.css': ['css/*.css', 'build/compiled.css']
         }
       }
     },
 
+    // minify js
     uglify: {
       build: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'build/resume.js',
+        dest: 'build/resume.min.js'
       }
     },
 
+    // minify css
     cssmin: {
       css: {
-        src: 'build/<%= pkg.name %>.css',
-        dest: 'build/<%= pkg.name %>.min.css'
+        src: 'build/resume.css',
+        dest: 'build/resume.min.css'
       }
     }
 
@@ -38,10 +51,11 @@ module.exports = function(grunt) {
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s)
-  grunt.registerTask('default', ['clean', 'concat', 'uglify', 'cssmin']);
+  grunt.registerTask('default', ['clean', 'less', 'concat', 'uglify', 'cssmin']);
 
 };
